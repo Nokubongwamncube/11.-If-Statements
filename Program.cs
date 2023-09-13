25d238dff4afde8ffdbd595e446c5a677b04f5bc
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-13 14:43:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            if (isMale)
+            {
+                Console.WriteLine("You are Male");
+            }
+            else
+            {
+                Console.WriteLine("You are Female");
+
+            }
+            bool isTall = true;
+            if (isTall && isMale)
+
+            {
+                Console.WriteLine("You are both Male and Tall");
+
+            }
+            else if (!isTall && isMale)
+            {
+                Console.WriteLine("You are a Short Male");
+
+            }
+            else if (!(isMale && isTall))
+            {
+                Console.WriteLine("You are neither a Male nor Tall");
+
+
+            }
+            else
+            {
+                Console.WriteLine("You are a women");
+
+            }
+
+            Console.Read();
+
+
+
+               
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
